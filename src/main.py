330319fd42c1f30516fce6de7c126b281da7a832
raylin009 <raylin009@gmail.com>
2023-06-20 18:42:21 -0700

    function name change experiment to main:

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ from fakeDataAPI import getFakeData
 from firebase import writeToFirebase
 from test import test_data_in_firestore
 
-def experiment (query, id):
+def main (query, id):
   doc = f'{query}{id}'
   data = getFakeData(query, id)
   writeToFirebase(query, doc, data)
   test_data_in_firestore([query, doc], data)
 
-experiment('posts', 6)
+main('posts', 6)
 
