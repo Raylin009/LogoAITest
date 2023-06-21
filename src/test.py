7d0b5475d7e71f8184b3dfb2f9ff3baa2039043a
raylin009 <raylin009@gmail.com>
2023-06-20 18:28:15 -0700

    added test attempt

--- src/test.py
+++ src/test.py
@@ -1,4 +1,27 @@
-from firebase import getDataFromFirebase
+from firebase import getDataFromFirebase, writeToFirebase
+from fakeDataAPI import getFakeData
+import unittest
+
+class TestWriteToDatabase(unittest.TestCase): 
+  def setup(self):
+    print('setUp')
+    # get fake datat
+    fake_data = getFakeData('posts', 6)
+    # write fake data to firestore
+    writeToFirebase('posts', f'posts{6}', fake_data)
+  
+  def tearDown(self):
+    print ('tearDown\n')
+    
+
+  def test_data(self):
+    # get fake datat
+    fake_data = getFakeData('posts', 6)
+    # retrieve data from firestore
+    fireStore_data = getDataFromFirebase('posts', 6)
+    # asssert data are correct
+    self.assertEqual(fireStore_data, fake_data)
+
 
 def test_data_in_firestore (quary_info, expected_data):
   [collection, doc] = quary_info
